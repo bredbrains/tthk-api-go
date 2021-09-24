4923d768d09a44a8c7141fb01b48bbb3a2a6a1b8
Nikolas Laus <bchik3@gmail.com>
2021-09-24 13:49:50 +0300

    Add documentation to User model.

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 import "gorm.io/gorm"
 
-// User Model to submit
+// User Model to submit data about user's group and social identities.
 type User struct {
 	gorm.Model
 	ID          int `gorm:"PrimaryKey"`
