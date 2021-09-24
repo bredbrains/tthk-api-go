4feac103218ac45ceaad2defca1d82c1173d976e
Nikolas Laus <bchik3@gmail.com>
2021-09-24 13:37:15 +0300

    Add links of departments' consultations to globals.

---
+++ utils/globals/consultations/departments.go
@@ -0,0 +1,11 @@
+package consultations
+
+const (
+	General               = "https://www.tthk.ee/oppetoo/opetajate-konsultatsioonid/uldainete-konsultatsioonid/"
+	Transport             = "https://www.tthk.ee/oppetoo/opetajate-konsultatsioonid/transporditehnika-valdkonna-konsultatsioonid/"
+	Mechanics             = "https://www.tthk.ee/oppetoo/opetajate-konsultatsioonid/mehaanika-ja-metallitootluse-valdkonna-konsultatsioonid/"
+	Energy                = "https://www.tthk.ee/oppetoo/opetajate-konsultatsioonid/mehhatroonka-osakonna-konsultatsiooid/"
+	InformationTechnology = "https://www.tthk.ee/infotehnoloogia-valdkonna-konsultatsioonid/"
+	Logistics             = "https://www.tthk.ee/logistika-valdkonna-konsultatsioonid/"
+	TextileSales          = "https://www.tthk.ee/oppetoo/opetajate-konsultatsioonid/tekstiili-ja-kaubanduse-valdkonna-konsultatsioonid/"
+)
