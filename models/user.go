359ecd9eab39c0f03374038fcb2d312e45955033
Nikolas Laus <bchik3@gmail.com>
2021-09-24 13:49:23 +0300

    Add Group field to User model.

--- models/user.go
+++ models/user.go
@@ -2,10 +2,12 @@ package models
 
 import "gorm.io/gorm"
 
+// User Model to submit
 type User struct {
 	gorm.Model
 	ID          int `gorm:"PrimaryKey"`
 	VKontakteID int
 	TelegramID  int
 	DiscordID   int
+	Group       string
 }
