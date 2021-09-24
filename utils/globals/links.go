74ec16d27d1a92371a146cea5ac275387a9e3d63
Nikolas Laus <bchik3@gmail.com>
2021-09-24 13:33:59 +0300

    Fix line in links.go from http to https.

--- utils/globals/links.go
+++ utils/globals/links.go
@@ -3,6 +3,6 @@ package globals
 type StaticLink string
 
 const (
-	Changes = "http://www.tthk.ee/tunniplaani-muudatused/"
+	Changes = "https://www.tthk.ee/tunniplaani-muudatused/"
 	Groups  = "https://www.tthk.ee/oppetoo/tunniplaan/ruhmajuhatajad/"
 )
